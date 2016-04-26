06248a983ff8bc5f64a244cba46b490c2f81e3c1
MohamedSerroukh <mohamed.serroukh@cesim.ac.ma>
2016-04-26 16:19:47 +0100

    Added files via upload

---
+++ calculs.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class calculs : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label4.Text = (Int32.Parse(TextBox1.Text) + Int32.Parse(TextBox2.Text)).ToString();
+        DataSetTableAdapters.CALCULTableAdapter tab = new DataSetTableAdapters.CALCULTableAdapter();
+        tab.AJOUTER(Int32.Parse(TextBox1.Text), Int32.Parse(TextBox2.Text), Int32.Parse(Label4.Text));
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+}
